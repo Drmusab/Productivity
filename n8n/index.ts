11c30676b972021d8f4ff0f771c013b472ac11f2
Drmusab <114311690+Drmusab@users.noreply.github.com>
2025-12-24 18:28:13 +0300

    Delete n8n directory

--- n8n/index.ts
+++
@@ -1,7 +0,0 @@
-import { KanbanApp } from './nodes/KanbanApp/KanbanApp.node';
-import { KanbanAppTrigger } from './nodes/KanbanApp/KanbanAppTrigger.node';
-import { KanbanAppApi } from './credentials/KanbanAppApi.credentials';
-
-export const nodes = [KanbanApp, KanbanAppTrigger];
-export const credentials = [KanbanAppApi];
-
